0edadf85372474f2ca787e3eaa50084bd7ff8860
Andrew Rob <andrew-c-rob@live.com>
2018-05-01 16:45:30 -0500

    A process for solving the problem has been outlined

--- distance-to-bun/distance-to-bun.js
+++ distance-to-bun/distance-to-bun.js
@@ -25,3 +25,59 @@
 
   (adapted from a codejam 2018 problem)
 */
+
+/*
+Prompt Says:
+1. Given an array of ingredients (strings) and 1 indgredient (string)  1. ['cheese', 'burger', 'tomato'], 'cheese'
+2. Returns number (how far the indgredient is to a bun) 2.  0
+
+Understanding: Given 1 array of ingredients, create counter, iterate through ingredients array, if passed in ingredient is at index 0 or ingredients array.length -1 count is 0, otherwise increment count, return count
+
+if arr length is odd: get index of last item, divide that by 2, if indexOf passed in ingredient is > than midpoint: indexOf last item in arr - indexOf passed in ingredient. if indexOf passed in ingredient is === midpoint: count is indexOf passed ingredient, if indexOf passed in ingredient is < than midpoint: count is indexOf passed in ingredient
+
+if arr length is even: get index of last item, divide that by 2, if indexOf passed in ingredient is > than midpoint: count is indexOf last item in arr - indexOf passed in ingredient. if indexOf passed in ingredient is < than midpoint: count is indexOf passed in ingredient
+
+Transformation:
+['cheese', 'burger', 'mayo', tomato'] => 3, 3/2 = 1.5
+passed in ingredient: 'mayo' => 2, 2 > 1.5, 3 - 2 = 1
+return 1
+
+['cheese', 'burger', 'mayo', tomato'] => 3, 3/2 = 1.5
+passed in ingredient: 'burger' => 1, 1 < 1.5, 1
+return 1
+
+['cheese', 'burger', 'bacon', 'mayo', 'lettuce', 'tomato'] => 5, 5/2 = 2.5
+passed in ingredient: 'mayo' => 3, 3 > 2.5 => 5 - 3 = 2 
+return 2
+
+['cheese', 'burger', 'mayo', 'lettuce', 'tomato'] => 4, 4 / 2 = 2
+passed in ingredient: 'lettuce' => 3, 3 > 2 => 4 - 3 = 1
+retrun 1
+
+['cheese', 'burger', 'tomato'] => 2, 2/2 = 1 
+passed in ingredient: 'burger' => 1, 1 === 1
+return 1
+
+['cheese', 'burger', 'bacon', 'pickles', 'mayo', 'lettuce', 'tomato'] => 4, 4/2 = 2
+passed in ingredient: 'burger' => 1, 1 < 2 => 1
+return 1
+*/
+
+function distanceToBun(arr, ingredient) {
+  //create variable to store distance
+  //create midpoint variable
+  //store index of last item in arr in var
+  //store index of ingredient in var
+  //calculate midpoint: index of last item in arr / 2
+  //if arr length is odd
+    //if index of ingredient is greater than midpoint
+      //distance is index of last item in arr - index of ingredient
+    //else 
+      //distance is index of ingredient
+  //if arr length is even
+    //if index of ingredient is greater than midpoint
+      //distance is index of last item in arr - index of ingredient
+    //else
+      //distance is index of ingredient
+  //return distance
+}
